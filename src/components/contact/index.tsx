d720cdd70bea2e02ce84c03cca364daf4f5a5778
aidin-rezaei <aydinrezaeefdr@gmail.com>
2023-12-07 09:05:43 -0800

    update src/components/contact/index.tsx

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -4,20 +4,20 @@ import { Tilt } from 'react-tilt'
 
 // import Tilt from 'react-vanilla-tilt';
 const Contact = () => {
-    const defaultOptions: any = {
-        reverse: false,  // reverse the tilt direction
-        max: 100,     // max tilt rotation (degrees)
-        perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
-        scale: 1.05,    // 2 = 200%, 1.5 = 150%, etc..
-        speed: 1000,   // Speed of the enter/exit transition
-        transition: true,   // Set a transition on enter/exit.
-        axis: null,   // What axis should be disabled. Can be X or Y.
-        reset: true,    // If the tilt effect has to be reset on exit.
-        easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-    }
+    // const defaultOptions: any = {
+    //     reverse: false,  // reverse the tilt direction
+    //     max: 100,     // max tilt rotation (degrees)
+    //     perspective: 1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    //     scale: 1.05,    // 2 = 200%, 1.5 = 150%, etc..
+    //     speed: 1000,   // Speed of the enter/exit transition
+    //     transition: true,   // Set a transition on enter/exit.
+    //     axis: null,   // What axis should be disabled. Can be X or Y.
+    //     reset: true,    // If the tilt effect has to be reset on exit.
+    //     easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+    // }
     return (
-        <>
-            <div className="relative flex justify-center items-center">
+        <div className=" w-full flex justify-center items-center flex-col">
+            <div className="w-full relative flex justify-center items-center">
                 <div className="absolute max-w-[700px] w-full h-full">
                     <div className="absolute top-[40%] left-[10%] before:shadow-[0_0_70px_50px_rgba(158,0,255,0.9)]"></div>
                     <div className="absolute top-[70%] left-[20%] before:shadow-[0_0_100px_80px_rgba(0,209,255,0.64)]"></div>
@@ -89,10 +89,21 @@ const Contact = () => {
 
 </div> */}
             </div>
-            <div className="pt-10 max-w-[700px] w-full">
-
+            <div className="relative z-10 pt-16 max-w-[700px] w-full grid grid-cols-2 grid-rows-6 gap-5">
+                <div className=' w-full'>
+                    <input className="text-sm p-2 w-full rounded-lg" type="text" name="" id="" placeholder="Full Name" />
+                </div>
+                <div className=' w-ful'>
+                    <input className="text-sm p-2 w-full rounded-lg" type="text" name="" id="" placeholder="Enter E-mail" />
+                </div>
+                <div className='col-span-2 w-ful'>
+                    <input className="text-sm p-2 w-full rounded-lg" type="text" name="" id="" placeholder="subject" />
+                </div>
+                <div className='col-span-2 row-span-4 w-full'>
+                    <textarea className="text-sm p-2 w-full h-full resize-none rounded-lg" name="" id="" placeholder="message"></textarea>
+                </div>
             </div>
-        </>
+        </div>
 
     );
 }
